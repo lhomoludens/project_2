d1fd19814313641fc564f74d2eb16e371802caaf
SeanTheJedi <richmondgyimah5@gmail.com>
2025-04-17 16:43:24 -0400

    adding gallery page heading

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,5 +1,11 @@
 import React, { useState }  from "react";
 
 export default function Gallery() {
-    
+    return(
+        <section id="video-gallery" class="gallery-section">
+            <article class="container mt-6 has-text-centered">
+                <h5 class="title is-centered is-size-4">Video Gallery</h5>
+            </article>
+        </section>
+    )
 }
