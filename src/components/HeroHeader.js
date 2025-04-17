b5b223ec5285401a5d4e77729d466a5fe244400f
lhomoludens <ysmnmlkc@gmail.com>
2025-04-17 02:03:55 -0400

    the button created
    The "Book now" button in the header part was created

--- src/components/HeroHeader.js
+++ src/components/HeroHeader.js
@@ -7,7 +7,9 @@ const HeroHeader = () => {
             <section className="hero-body">
                 <h1 className="title">Welcome to YCS Travel Agency</h1>
                 <p className="subtitle mt-2">Explore the world with us and find your perfect getaway.</p>
-
+                <Link to="/contact" className="button is-light has-text-weight-bold is-rounded">
+                    Book Now!
+                </Link>
             </section>
         </header>
     );
