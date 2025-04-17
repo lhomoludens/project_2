91cb31c551daa1cc1cdb61e8e9c86c51d0c1c241
lhomoludens <ysmnmlkc@gmail.com>
2025-04-17 02:01:21 -0400

    created HeroHeader component
    "h1" and "p" parts added to header tag

--- src/components/HeroHeader.js
+++ src/components/HeroHeader.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import 'bulma/css/bulma.min.css'
+
+const HeroHeader = () => {
+    return (
+        <header className="hero is-small is-primary has-text-centered">
+            <section className="hero-body">
+                <h1 className="title">Welcome to YCS Travel Agency</h1>
+                <p className="subtitle mt-2">Explore the world with us and find your perfect getaway.</p>
+
+            </section>
+        </header>
+    );
+};
