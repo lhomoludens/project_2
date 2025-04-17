508655182507432c9e100826e2cf4a29ca5d4f84
lhomoludens <ysmnmlkc@gmail.com>
2025-04-17 02:15:28 -0400

    defined Navbar function
    navigation part made, useState and toggle menu were created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,2 +1,21 @@
 import React, { useState } from 'react';
 import 'bulma/css/bulma.min.css'
+
+const Navbar = () => {
+    const [isActive, setIsActive] = useState(false);
+
+    const toggleMenu = () => {
+        setIsActive(!isActive);
+    };
+
+    return (
+        <nav className="navbar has-background-dark has-text-centered" role="navigation" aria-label="main navigation">
+            <section className="container">
+                <div className="navbar-brand">
+                    <Link className="navbar-item" to="/">
+                        <strong>LOGO</strong>
+                    </Link>
+            </section>
+        </nav>
+    );
+};
