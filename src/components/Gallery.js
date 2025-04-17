4d4bbac9b96d6ff01d5dbe62494a9f373aa4db3d
SeanTheJedi <richmondgyimah5@gmail.com>
2025-04-17 16:24:05 -0400

    declaring state variables

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,5 +1,12 @@
 import React, { useState }  from "react";
 
 export default function Gallery() {
-   
+    const [formData, setFormData] = useState({ name: '', email: '', msg: '' });
+    const [errors, setErrors] = useState({ name: '', email: '', msg: '' });
+  
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.id]: e.target.value });
+        setErrors({ ...errors, [e.target.id]: '' }); // Clear error on change
+    };
+    
 }
