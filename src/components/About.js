9f66ceb581370c1f6f8089385742e757003a8f1d
lhomoludens <ysmnmlkc@gmail.com>
2025-04-17 01:45:43 -0400

    Created About component
    Defined About function with const in About page, added title and description

--- src/components/About.js
+++ src/components/About.js
@@ -1,2 +1,15 @@
 import React from "react";
 import 'bulma/css/bulma.min.css'
+
+
+const About = () => {
+    return (
+        <main className="container">
+            <h1 className="title has-text-centered is-size-4 mt-5">About Us</h1>
+
+            <section className="content has-text-centered">
+                <p>Founded in 2015, YCS Travel Agency is dedicated to helping travelers explore the world with ease and confidence. Our mission is to provide unforgettable travel experiences by offering personalized packages, expert guidance, and seamless trip planning. Whether you're looking for adventure, relaxation, or cultural exploration, we are committed to making your journey exceptional.</p>
+            </section>
+        </main>
+    );
+};
