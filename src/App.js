25d1cc5e0d28206690fc029670ec53bc6121710b
lhomoludens <ysmnmlkc@gmail.com>
2025-04-17 02:31:24 -0400

    the created components were called
    app.js page was cleaned and the created components are added to the router tags

--- src/App.js
+++ src/App.js
@@ -7,22 +7,14 @@ import 'bulma/css/bulma.min.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <HeroHeader />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
