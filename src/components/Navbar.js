3a516fb58d23e4a62b22f84b9790d9d6d2424f8f
lhomoludens <ysmnmlkc@gmail.com>
2025-04-17 02:20:13 -0400

    drop down menu created
    Added button for drop down menu on mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,6 +15,22 @@ const Navbar = () => {
                     <Link className="navbar-item" to="/">
                         <strong>LOGO</strong>
                     </Link>
+
+                    <Link
+                        role="button"
+                        className={`navbar-burger has-text-weight-bold has-text-success ${isActive ? 'is-active' : ''}`}
+                        aria-label="menu"
+                        aria-expanded={isActive ? 'true' : 'false'}
+                        onClick={toggleMenu}
+                    >
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </Link>
+                </div>
+
+
+
             </section>
         </nav>
     );
