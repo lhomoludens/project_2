f39914810b23f56e2d352fd97097e5fa30d0250c
lhomoludens <ysmnmlkc@gmail.com>
2025-04-17 02:23:15 -0400

    navbar menu items added
    navbar menu items were created, "a" tags were updated as links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,8 +29,14 @@ const Navbar = () => {
                     </Link>
                 </div>
 
-
-
+                <div className={`navbar-menu ${isActive ? 'is-active' : ''}`}>
+                    <div className="navbar-start">
+                        <Link className="navbar-item ml-6 mr-6 has-text-success has-text-weight-bold is-hoverable" to="/">Home</Link>
+                        <Link className="navbar-item ml-6 mr-6 has-text-success has-text-weight-bold is-hoverable" to="/about">About</Link>
+                        <Link className="navbar-item ml-6 mr-6 has-text-success has-text-weight-bold is-hoverable" to="/contact">Contact</Link>
+                        <Link className="navbar-item ml-6 mr-6 has-text-success has-text-weight-bold is-hoverable" to="/gallery">Gallery</Link>
+                    </div>
+                </div>
             </section>
         </nav>
     );
