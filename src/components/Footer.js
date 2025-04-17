c53933f7e823d5fe86346699e13fa36adbcfe73a
Carlaalexa12 <mirandavumi8@gmail.com>
2025-04-17 14:07:45 -0400

    Created basic Footer component structure with props and state setup

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,11 @@
 import React, { useState } from "react";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bulma/css/bulma.min.css";
+
+const Footer = ({ links, year }) => {
+   
+    const [socialLinks, setSocialLinks] = useState(links);
+
+};
+
+export default Footer;
