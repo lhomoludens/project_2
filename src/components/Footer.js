fed5d4b04ceb899e4a4a840afcb527e5c3d1f9bc
Carlaalexa12 <mirandavumi8@gmail.com>
2025-04-17 14:14:03 -0400

    Render the social media icons in Footer using props

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,28 @@ const Footer = ({ links, year }) => {
    
     const [socialLinks, setSocialLinks] = useState(links);
 
+    return (
+        <footer className="footer has-background-primary mt-6 py-6">
+          <div className="content has-text-centered">
+            <h2 className="title has-text-white">Follow us!</h2>
+    
+            {/* Render from state */}
+            {socialLinks.map((link, idx) => (
+              <a
+                key={idx}
+                href={link.href}
+                target="_blank"
+                rel="noreferrer"
+                className="mx-2"
+              >
+                <i className={`${link.icon} fa-2x has-text-blue`}></i>
+              </a>
+            ))}
+    
+          </div>
+        </footer>
+      );
+
 };
 
 export default Footer;
