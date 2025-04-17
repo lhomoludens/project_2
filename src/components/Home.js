5f3183acf8a621365b12c5931b62b1055b3c0221
Carlaalexa12 <mirandavumi8@gmail.com>
2025-04-17 14:36:47 -0400

    Created the Destinations section to display cards dynamically using destinationCards prop

--- src/components/Home.js
+++ src/components/Home.js
@@ -41,6 +41,35 @@ const Home = ({ homeCards, destinationCards, tourCards }) => {
             </article>
           </div>
         </section>
+
+        <section id="destination" className="destinations-section">
+        <div className="container">
+          <h3 className="title is-size-4 has-text-centered mb-5">
+            Popular Destinations
+          </h3>
+          <p className="subtitle is-size-6 has-text-centered mb-5">
+            Find the best travel spots for your next trip.
+          </p>
+          <article className="columns mt-5 is-8 is-variable">
+            {destinationCards.map((item, index) => (
+              <div className="column is-1-tablet is-4-desktop" key={index}>
+                <div className="card">
+                  <div className="card-image">
+                    <figure className="image is-4by3">
+                      <img src={item.image} alt={item.alt} />
+                    </figure>
+                  </div>
+                  <div className="card-content">
+                    <p className="subtitle has-text-weight-bold has-text-centered is-size-6 pb-3">
+                      {item.text}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </article>
+        </div>
+      </section>
       </>
     );
   };
