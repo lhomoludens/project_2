88314e41d29094235e1397c3ab4834ec4abc84e6
SeanTheJedi <richmondgyimah5@gmail.com>
2025-04-17 16:44:12 -0400

    adding gallery body elements

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -6,6 +6,13 @@ export default function Gallery() {
             <article class="container mt-6 has-text-centered">
                 <h5 class="title is-centered is-size-4">Video Gallery</h5>
             </article>
+            <p class="subtitle is-centered is-size-6 mt-3">Discover Jordan, Our New Travel Destination.</p>
+            <div class="video has-ratio is-16by9">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/9vN_htf-05w?si=UzxvZU7_4JiqBNGY"
+                    title="YouTube video player" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
         </section>
     )
 }
