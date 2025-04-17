469c2500a5d2e64801bb371a89608f96ceb5d70c
lhomoludens <ysmnmlkc@gmail.com>
2025-04-17 01:48:23 -0400

    Added image
    Image was placed inside figure tags

--- src/components/About.js
+++ src/components/About.js
@@ -10,6 +10,13 @@ const About = () => {
             <section className="content has-text-centered">
                 <p>Founded in 2015, YCS Travel Agency is dedicated to helping travelers explore the world with ease and confidence. Our mission is to provide unforgettable travel experiences by offering personalized packages, expert guidance, and seamless trip planning. Whether you're looking for adventure, relaxation, or cultural exploration, we are committed to making your journey exceptional.</p>
             </section>
+            <figure className="image is-256x256">
+                <img
+                    className="is-rounded"
+                    src="https://images.unsplash.com/photo-1590650589327-3f67c43ad8a2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    alt="About Us"
+                />
+            </figure>
         </main>
     );
 };
